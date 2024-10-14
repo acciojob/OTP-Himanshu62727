f598b4d85b8f8b020b3f0cd54efb471809c7edbe
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2024-10-14 10:40:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+    input.addEventListener('keydown', (e) => {
+        // Move to next input when typing a number
+        if (e.key >= 0 && e.key <= 9) {
+            inputs[index].value = ''; // Clears the input
+            setTimeout(() => {
+                if (index < inputs.length - 1) {
+                    inputs[index + 1].focus();
+                }
+            }, 10);
+        } else if (e.key === 'Backspace') {
+            // Move to previous input on Backspace
+            setTimeout(() => {
+                if (index > 0) {
+                    inputs[index - 1].focus();
+                }
+            }, 10);
+        }
+    });
+
+    input.addEventListener('input', () => {
+        if (input.value.length === 1 && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+});
